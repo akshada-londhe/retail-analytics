812031ca5f1a1b6ee04387d923e5c50912395f25
Akshada-londhe <akshadalondhe07@gmail.com>
2026-07-18 07:44:20 -0700

    Fix/UI

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 dashboard = Blueprint('dashboard', __name__)
 
-
 @dashboard.route('/')
 def view_dashboard():
-    """Display dashboard."""
+    print("Dashboard Session:", dict(session))
     return render_template('dashboard.html')
